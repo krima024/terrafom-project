60b298bc43e6dc1d14c63def8f99c6c843a4c627
krima024 <99860803+krima024@users.noreply.github.com>
2022-05-11 18:26:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
     region = "us-east-1"
 }
 module "vpc" {
-    source = "../module/vpc"
+    source = "./module/vpc"
    
 }
 
 module "ec2" {
-    source = "../module/ec2"
+    source = "./module/ec2"
     subnet_id = module.vpc.subnet_id
     security_group = module.vpc.security_group
 }
