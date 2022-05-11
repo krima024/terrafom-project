1ecff84357275381aeb16ea2f902471ec6f2deb7
krima024 <99860803+krima024@users.noreply.github.com>
2022-05-11 17:51:45 +0530

    Update instance.tf

--- module/ec2/instance.tf
+++ module/ec2/instance.tf
@@ -1,4 +1,5 @@
-resource "aws_instance" "IP_example" {
+
+resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
@@ -7,7 +8,7 @@ resource "aws_instance" "IP_example" {
   # security_groups = "${var.security_group}"
  # private_ip = "10.0.1.10"
   # key name
-  key_name = var.key_name
+  key_name = "RDSKEY"
 
   user_data = <<EOF
     #! /bin/bash
@@ -23,29 +24,48 @@ resource "aws_instance" "IP_example" {
   }
 }
 
-resource "aws_eip" "eip" {
-  instance = aws_instance.IP_example.id
+# route53  
+resource "aws_route53_zone" "easy_aws" {
+  name = "manhardangar.ml"
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.easy_aws.zone_id
+  name    = "www.manhardangar.ml"
+  type    = "A"
+  ttl     = "30"
+
+  #records = module.ec2.aws_eip
+  records = [aws_eip.lb.public_ip]
+  #records = aws_route53_zone.easy_aws.name_servers
+}
+
+
+# output "name_server"{
+#   value=aws_route53_zone.easy_aws.name_servers
+# }
+resource "aws_eip" "lb" {
+  instance = aws_instance.web.id
   vpc      = true
 }
-output "aws_eip" {
-  value = aws_eip.eip.public_ip
-}
-output "public_ip" {
-  value = aws_instance.IP_example.public_ip
-}
 
-output "ec2_arn" {
-  value = aws_instance.IP_example.arn
-}
-output "ec2_instance_state" {
-  value = aws_instance.IP_example.instance_state
-}
-output "ec2_public_dns" {
-  value = aws_instance.IP_example.public_dns
-}
-
-# output "subnet_id" {
-#   value = aws_subnet.public-1.id
+# output "aws_eip" {
+#   value = aws_eip.lb.public_ip
+# }
+# output "public_ip" {
+#   value = aws_instance.web.public_ip
 # }
 
-
+# output "ec2_arn" {
+#   value = aws_instance.web.arn
+# }
+# output "ec2_instance_state" {
+#   value = aws_instance.web.instance_state
+# }
+# output "ec2_public_dns" {
+#   value = aws_instance.web.public_dns
+# }
